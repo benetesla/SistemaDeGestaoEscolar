bfd9facac0368cbe795064e642789fae134a97d5
benetesla <78994881+benetesla@users.noreply.github.com>
2022-10-21 12:17:11 -0300

    Sistema de gestao

--- controle.java
+++ controle.java
@@ -5,7 +5,7 @@ public class controle {
   public static void main(String[] args) {
     JOptionPane.showMessageDialog(
       null,
-      "Bem vindo ao sistema de controle de alunos, professores e diretores da Universidade Federal de Pernambuco"
+      "Bem vindo ao sistema de controle de alunos, professores e diretores da Universidade Federal do Estado de Sergipe"
     );
     String nome = JOptionPane.showInputDialog("Digite o nome do aluno");
     String matricula = JOptionPane.showInputDialog(
@@ -19,6 +19,18 @@ public class controle {
     JOptionPane.showMessageDialog(
       null,
       "O aluno " + aluno1.getNome() + " foi cadastrado com sucesso"
+    );
+    //exibir dados do aluno
+    JOptionPane.showMessageDialog(
+      null,
+      "Nome: " +
+      aluno1.getNome() +
+      " Matricula: " +
+      aluno1.getMatricula() +
+      " Curso: " +
+      aluno1.getCurso() +
+      " Departamento: " +
+      aluno1.getDepartamento()
     );
     String nome2 = JOptionPane.showInputDialog("Digite o nome do professor");
     String id = JOptionPane.showInputDialog("Digite o id do professor");
@@ -48,6 +60,26 @@ public class controle {
       null,
       "O professor " + professor1.getNome() + " foi cadastrado com sucesso"
     );
+    //exibir dados do professor
+    JOptionPane.showMessageDialog(
+      null,
+      "Nome: " +
+      professor1.getNome() +
+      " Id: " +
+      professor1.getId() +
+      " Departamento: " +
+      professor1.getDepartamento() +
+      " Curso: " +
+      professor1.getCurso() +
+      " Disciplina: " +
+      professor1.getDisciplina() +
+      " Turma: " +
+      professor1.getTurma() +
+      " Horario: " +
+      professor1.getHorario() +
+      " Sala: " +
+      professor1.getSala()
+    );
     String nome3 = JOptionPane.showInputDialog("Digite o nome do diretor");
     String id2 = JOptionPane.showInputDialog("Digite o id do diretor");
     String departamento3 = JOptionPane.showInputDialog(
@@ -58,6 +90,54 @@ public class controle {
     JOptionPane.showMessageDialog(
       null,
       "O diretor " + diretor1.getNome() + " foi cadastrado com sucesso"
+    );
+    //exibir dados do diretor
+    JOptionPane.showMessageDialog(
+      null,
+      "Nome: " +
+      diretor1.getNome() +
+      " Id: " +
+      diretor1.getId() +
+      " Departamento: " +
+      diretor1.getDepartamento() +
+      " Curso: " +
+      diretor1.getCurso()
+    );
+    //exibir todos os dados da universidade
+    JOptionPane.showMessageDialog(
+      null,
+      "Nome: " +
+      aluno1.getNome() +
+      " Matricula: " +
+      aluno1.getMatricula() +
+      " Curso: " +
+      aluno1.getCurso() +
+      " Departamento: " +
+      aluno1.getDepartamento() +
+      " Nome\n: " +
+      professor1.getNome() +
+      " Id: " +
+      professor1.getId() +
+      " Departamento: " +
+      professor1.getDepartamento() +
+      " Curso: " +
+      professor1.getCurso() +
+      " Disciplina: " +
+      professor1.getDisciplina() +
+      " Turma: " +
+      professor1.getTurma() +
+      " Horario: " +
+      professor1.getHorario() +
+      " Sala: " +
+      professor1.getSala() +
+      " Nome\n: " +
+      diretor1.getNome() +
+      " Id: " +
+      diretor1.getId() +
+      " Departamento: " +
+      diretor1.getDepartamento() +
+      " Curso\n: " +
+      diretor1.getCurso()
     );
   }
 }
